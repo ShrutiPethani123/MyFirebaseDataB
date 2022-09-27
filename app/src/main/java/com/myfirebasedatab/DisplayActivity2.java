c02b2f4853226093062154aea2bfac9361243db1
= <=>
2022-09-27 13:08:17 +0530

    empty activity

---
+++ app/src/main/java/com/myfirebasedatab/DisplayActivity2.java
@@ -0,0 +1,14 @@
+package com.myfirebasedatab;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DisplayActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display2);
+    }
+}
